52a31e32f48aa90b02a9c749d1af699f33a6c38b
HoJin9622 <kiss0104040@gmail.com>
2022-11-03 19:51:00 +0900

    feat: 가계부 기록 api 생성

--- households/views.py
+++ households/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED
+from .serializers import HouseholdSerializer
 
-# Create your views here.
+
+class HouseholdDetailView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        """
+        사용한 돈의 금액과 관련된 메모를 남깁니다.
+        POST api/v1/households/
+        """
+
+        serializer = HouseholdSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({"ok": True}, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
