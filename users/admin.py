a9a29ffc50edce3b299bf529f4952124dae4a8cd
HoJin9622 <kiss0104040@gmail.com>
2022-11-03 12:35:06 +0900

    feat: 유저 어드민 생성

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "프로필",
+            {
+                "fields": (
+                    "email",
+                    "password",
+                    "username",
+                ),
+                "classes": ("wide",),
+            },
+        ),
+        (
+            "권한",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "username"),
+            },
+        ),
+    )
+    list_display = ("email", "username")
